a54238ab34d5de547728d94dfc1fb7cc89510d70
chaehyeon7 <81363193+chaehyeon7@users.noreply.github.com>
2021-08-24 19:39:30 +0900

    Edit Add MusicService.java(메서드) Commit

--- app/src/main/java/kr/hs/emirim/chaehyeon/ex_14_1_4/MusicService.java
+++ app/src/main/java/kr/hs/emirim/chaehyeon/ex_14_1_4/MusicService.java
@@ -3,6 +3,7 @@ package kr.hs.emirim.chaehyeon.ex_14_1_4;
 import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
+import android.util.Log;
 
 public class MusicService extends Service {
     public MusicService() {
@@ -11,6 +12,39 @@ public class MusicService extends Service {
     @Override
     public IBinder onBind(Intent intent) {
         // TODO: Return the communication channel to the service.
-        throw new UnsupportedOperationException("Not yet implemented");
+        return null;
+    }
+
+    @Override
+    public void onCreate(){
+        Log.i("서비스 테스트::", "onCreate(); 호출됨");
+        super.onCreate();
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        Log.i("서비스 테스트::", "onStartCommand(); 호출됨");
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.i("서비스 테스트::", "onDestroy(); 호출됨");
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
